d5001fb33ef5203a55d41a619c4ef5e338115345
Michael Crosby <michael.crosby@moogsoft.com>
2022-06-16 10:04:03 -0700

    adding additional provider

--- main.tf
+++ main.tf
@@ -5,12 +5,30 @@ terraform {
       source  = "fmontezuma/restapi"
       version = "1.14.1"
     }
+    restapiV2 = {
+      source  = "fmontezuma/restapi"
+      version = "1.14.1"
+    }
   }
 }
 
 # Configure the provider
 provider "restapi" {
   uri                   = "https://api.moogsoft.ai/v2"
+  debug                 = true
+  write_returns_object  = true
+  create_returns_object = true
+  update_method         = "PATCH"
+  read_method           = "GET"
+  id_attribute          = "data/id"
+  headers = {
+    "Content-Type" : "application/json",
+    "apiKey" : var.apiKey
+  }
+}
+
+provider "restapiV2" {
+  uri                   = "https://api.moogsoft.ai"
   debug                 = true
   write_returns_object  = true
   create_returns_object = true
