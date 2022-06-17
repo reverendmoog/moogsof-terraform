c09a64a9787358af42787e5ae3d3980e56963c43
Richard Whitehead <richard@moogsoft.com>
2022-06-17 00:12:51 +0000

    Enable workflow with local-exec

--- workflow.tf
+++ workflow.tf
@@ -1,5 +1,5 @@
 #
-#
+# This config creates a workflow. A second action is required to enable it
 #
 
 # Create a workflow
@@ -26,4 +26,16 @@ resource "restapi_object" "test_workflow" {
       }
     ]
   })
+}
+
+resource "null_resource" "enable_test_workflow" {
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -o /tmp/curlout \
+        -XPATCH https://api.moogsoft.ai/v1/workflows/${restapi_object.test_workflow.id}/status \
+        -H 'Content-Type: application/json' \
+        -H 'apiKey: ${var.apiKey}' \
+        -d '{\"status\": \"RUNNING\"}'
+EOT
+  }
 }
