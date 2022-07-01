77e23f8f3eaf1e0371f0e87a9b0971ee72e5aed6
Richard Whitehead <richard@moogsoft.com>
2022-07-01 18:57:54 +0000

    Remove excess fields

--- correlation-definitions.tf
+++ correlation-definitions.tf
@@ -1,5 +1,5 @@
 #
-# This config creates a correlation definition
+# This config creates a correlation definition which is assigned to a default group
 #
 
 # Create a workflow
@@ -15,8 +15,6 @@ resource "restapi_object" "similar-services" {
     }
     correlation_time_period = 5400
     incident_description    = "Incident impacting unique(service) service on unique_count(source) nodes, of type ( unique(type) ) and class (unique(class)) "
-    created_by              = "richard@moogsoft.com"
     alert_threshold         = 2
-    group_id                = "d9a15208-2a75-431d-af27-44410ca98e2e"
   })
 }
